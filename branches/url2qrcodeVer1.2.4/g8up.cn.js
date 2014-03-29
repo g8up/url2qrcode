1fbeb8f3d97bac1b7a671c0ea300ecdb32ba25c7
maduxing@gmail.com <maduxing@gmail.com@3e9ce3bc-fe25-85ac-8411-060698df7652>
2014-03-29 08:58:59 +0000

    1.2.4:change logo， add url encode

--- branches/url2qrcodeVer1.2.4/g8up.cn.js
+++ branches/url2qrcodeVer1.2.4/g8up.cn.js
@@ -1,4 +1,4 @@
 chrome.tabs.getSelected( null , function ( tab ){
-	var url = "http://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|1&chl=" + tab.url;
+	var url = "http://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|1&chl=" + encodeURIComponent( tab.url );
 	document.querySelector('#qrcode').src = url;
 });
