cbd8ef8fb9b80e3ef39d98e9ac52847e82c928b9
maduxing@gmail.com <maduxing@gmail.com@3e9ce3bc-fe25-85ac-8411-060698df7652>
2014-06-26 16:55:44 +0000

    修改为本地转换图片

--- branches/url2qrcodeVer1.2.5/g8up.cn.js
+++ branches/url2qrcodeVer1.2.5/g8up.cn.js
@@ -1,4 +1,17 @@
+function makeCode ( qrcode, text ) {
+
+	if ( typeof text === 'undefined' ) {
+		return;
+	}
+	qrcode.makeCode( text );
+}
+
 chrome.tabs.getSelected( null , function ( tab ){
-	var url = "http://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|1&chl=" + encodeURIComponent( tab.url );
-	document.querySelector('#qrcode').src = url;
+	var qrcode = document.querySelector('#qrcode');
+	var text = ( tab.url );
+	var qrcode = new QRCode( qrcode , {
+		width : 300,
+		height : 300
+	});
+	makeCode( qrcode, text );
 });
