3bfbb0834b3405e3d73345e4ac9d9b88bc22d5d8
g8up <g8up@qq.com>
2019-03-19 20:56:25 +0800

    feat: 排序；删除

--- src/history.js
+++ src/history.js
@@ -7,10 +7,25 @@ class History {
   }
 
   render(){
-    return this.store.get().then( (list=[]) =>{
-      this.node.innerHTML = list.map( record=>{
-        return `<li>${record}</li>`;
-      }).join('');
+    return this.store.get().then( (data = {}) =>{
+      const list = Object.keys( data ).map( text => {
+        return {
+          text,
+          timestamp: data[text],
+        };
+      });
+      let html;
+      if( list.length ){
+        html = list.sort((t1, t2)=>{
+          return t2.timestamp - t1.timestamp;
+        }).slice().map( record=>{
+          return `<li><span class="delete">&times;</span>${record.text}</li>`;
+        }).join('');
+      }
+      else{
+        html = `<li>暂无数据</li>`;
+      }
+      this.node.innerHTML = html;
     });
   }
 
@@ -24,6 +39,23 @@ class History {
       } = target;
       handle( textContent );
     });
+  }
+
+  saveRecord( text ){
+    return this.store.get().then( (data = {}) =>{
+      data[text] = + new Date(); // 用于排序
+      return this.store.set(data);
+    });
   };
 
+  clearAll(){
+    return this.store.set({});
+  }
+
+  delete( text ){
+    return this.store.get().then( (data = {}) =>{
+      delete data[text];
+      return this.store.set(data);
+    });
+  }
 }
