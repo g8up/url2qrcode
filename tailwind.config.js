73ce8285de7a91909b9bdf02f7a4bc8712fa36e6
g8up <g8up@qq.com>
2022-07-14 12:04:14 +0800

    chore: 引入 tailwind 编译

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './crx/assets/*.html',
+    './crx/option.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
